d2e5279c5bb1deb93fced70facc7c4779d60d2d9
elaiwin <35325842+elaiwin@users.noreply.github.com>
2020-05-28 21:03:34 -0700

    added x,y,z sensor and updated IMU sensor

--- motion_imitation/envs/env_builder.py
+++ motion_imitation/envs/env_builder.py
@@ -42,9 +42,10 @@ def build_imitation_env(motion_files, num_parallel_envs, mode,
   robot_class = laikago.Laikago
 
   sensors = [
-      sensor_wrappers.HistoricSensorWrapper(wrapped_sensor=robot_sensors.MotorAngleSensor(num_motors=laikago.NUM_MOTORS), num_history=3),
-      sensor_wrappers.HistoricSensorWrapper(wrapped_sensor=robot_sensors.IMUSensor(), num_history=3),
-      sensor_wrappers.HistoricSensorWrapper(wrapped_sensor=environment_sensors.LastActionSensor(num_actions=laikago.NUM_MOTORS), num_history=3)
+      sensor_wrappers.HistoricSensorWrapper(wrapped_sensor=robot_sensors.BasePositionSensor(), num_history=3),
+      sensor_wrappers.HistoricSensorWrapper(wrapped_sensor=robot_sensors.IMUSensor(['R',
+        'P', 'Y', 'dR', 'dP', 'dY']), num_history=3),
+      sensor_wrappers.HistoricSensorWrapper(wrapped_sensor=robot_sensors.MotorAngleSensor(num_motors=laikago.NUM_MOTORS), num_history=3)
   ]
 
   task = imitation_task.ImitationTask(ref_motion_filenames=motion_files,
